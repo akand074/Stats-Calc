d29313aec4b5822bf91cdb509f0490c9a94c597d
Andrew Kandalaft <andrew@andrew-desktop.(none)>
2011-05-08 21:21:07 -0400

    Main activity started

--- gen/com/android/statscalc/R.java
+++ gen/com/android/statscalc/R.java
@@ -11,7 +11,20 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int basic=0x7f020000;
+        public static final int discrete=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int linreg=0x7f020003;
+        public static final int main_screen=0x7f020004;
+        public static final int permcomb=0x7f020005;
+    }
+    public static final class id {
+        public static final int basic=0x7f050001;
+        public static final int discrete=0x7f050005;
+        public static final int linreg=0x7f050002;
+        public static final int permComb=0x7f050004;
+        public static final int tableRow1=0x7f050000;
+        public static final int tableRow2=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;

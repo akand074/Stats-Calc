d99407342f59049c4f55ed489f9057796052ee44
Andrew Kandalaft <andrew@andrew-desktop.(none)>
2011-08-26 18:46:31 -0400

    Rewrote permcomb... somehow it got reverted. Working again.

--- src/com/aeidesign/statscalc/stats/PermComb.java
+++ src/com/aeidesign/statscalc/stats/PermComb.java
@@ -1,29 +1,31 @@
 package com.aeidesign.statscalc.stats;
 
+import java.math.BigInteger;
+
 public class PermComb {
 	
 	public static long calcPigeonhole(long set, long group) {
 		return set % group == 0? set/group : (set/group) + 1;
 	}
 
-	public static long calcNumSubset(long set) {
-		return (long) Math.pow(2, set);
+	public static String calcNumSubset(long set) {
+		return new BigInteger(String.valueOf(2)).pow((int)set).toString();
 	}
 
-	public static long calcCombinations(long set, long group) {
-		return Functions.calcFactorial(set).divide((Functions.calcFactorial(group).multiply(Functions.calcFactorial(set - group)))).longValue(); 
+	public static String calcCombinations(long set, long group) {
+		return Functions.calcFactorial(set).divide((Functions.calcFactorial(group).multiply(Functions.calcFactorial(set - group)))).toString(); 
 	}
 
-	public static long calcCombinationsWithRep(long set, long group) {
-		return Functions.calcFactorial(set + group - 1).divide((Functions.calcFactorial(group).multiply(Functions.calcFactorial(set - 1)))).longValue();
+	public static String calcCombinationsWithRep(long set, long group) {
+		return Functions.calcFactorial(set + group - 1).divide((Functions.calcFactorial(group).multiply(Functions.calcFactorial(set - 1)))).toString();
 	}
 
-	public static long calcPermutationsWithRep(long set, long group) {
-		return (long) Math.pow(set, group);
+	public static String calcPermutationsWithRep(long set, long group) {
+		return new BigInteger(String.valueOf(set)).pow((int)group).toString();
 	}
 
-	public static long calcPermutations(long set, long group) {
-		return Functions.calcFactorial(set).divide(Functions.calcFactorial(set - group)).longValue();
+	public static String calcPermutations(long set, long group) {
+		return Functions.calcFactorial(set).divide(Functions.calcFactorial(set - group)).toString();
 	}
 
 }
